6a8040278c2e5b98e0c10759b4dfa73e4a27426a
Tatiana Smolin <tatismolin@gmail.com>
2019-09-17 03:13:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,22 @@ end
 def using_uniq(array)
   array.using
 end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
+
+
+
+
+
+
